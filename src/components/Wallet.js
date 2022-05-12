4a4aa63a86436ac1658bd8ba5c79bb87b50cf7f6
Coltrane Yan <coltraneyan@gmail.com>
2022-05-11 22:07:43 -0400

    updated export styling and render quality

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { QRCodeSVG } from "qrcode.react";
 import { exportComponentAsJPEG } from "react-component-export-image";
+import html2canvas from "html2canvas";
 
 const Wallet = (props) => {
   const walletRef = useRef();
@@ -8,32 +9,42 @@ const Wallet = (props) => {
   if (props.address) {
     return (
       <div className="flex-column-align">
-        <div
-          ref={walletRef}
-          className={`card-basic div-center-h margin-top-2 ${props.style}`}
-        >
-          <div className="card-top flex-row-align disperse">
-            <div className="flex-column card-info">
-              <div>{props.name}</div>
-              <div>{props.blockchain}</div>
-            </div>
-            <div className="flex-column-align wide-font card-logo">
-              <div>Portal</div>
-              <div className="mono-font font-size-s border-round-xs border-thick pad-025">
-                wallet
+        <div ref={walletRef} className="div-center-h margin-top-2 pad-2">
+          <div className={`card-basic div-center-h ${props.style}`}>
+            <div className="card-top flex-row-align disperse">
+              <div className="flex-column card-info">
+                <div>{props.name}</div>
+                <div>{props.blockchain}</div>
+              </div>
+              <div className="flex-column-align wide-font card-logo">
+                <div>Portal</div>
+                <div className="mono-font font-size-s border-round-xs border-thick pad-025">
+                  wallet
+                </div>
               </div>
             </div>
-          </div>
-          <div className="card-bottom">
-            <div className="card-address">{props.address}</div>
-            <div className="card-QR-wrapper">
-              <QRCodeSVG className="card-QR" value={props.address} size={60} />
+            <div className="card-bottom">
+              <div className="card-address">{props.address}</div>
+              <div className="card-QR-wrapper">
+                <QRCodeSVG
+                  className="card-QR"
+                  value={props.address}
+                  size={60}
+                />
+              </div>
             </div>
           </div>
         </div>
         <div
           className="margin-top-2 border-round-s responsive-size pad-1 info-input font-color-white wide-font font-size-s"
-          onClick={() => exportComponentAsJPEG(walletRef)}
+          onClick={() =>
+            exportComponentAsJPEG(walletRef, {
+              html2CanvasOptions: {
+                backgroundColor: "#303030",
+                scale: 2 * window.devicePixelRatio,
+              },
+            })
+          }
         >
           Download Wallet
         </div>
